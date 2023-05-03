8d7288f49d65a48c4091aa0ebae2cc5926e79de8
rutkovsky <rus.rutkovsky@yandex.ru>
2023-05-03 20:46:29 +0300

    microtasks -button

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
-import React from 'react';
+import React, {MouseEvent} from 'react';
 import './App.css';
-import {NewComponent} from './NewComponent';
-import {TopCars} from './TopCars';
+import {Button} from './Components/Button';
 
 const App = () => {
-    const topCars = [
-        {manufacturer:'BMW', model:'m5cs'},
-        {manufacturer:'Mercedes', model:'e63s'},
-        {manufacturer:'Audi', model:'rs6'}
-    ]
-    const students = [
-        {id: 1, name: 'James', age: 8},
-        {id: 2, name: 'Robert', age: 18},
-        {id: 3, name: 'John', age: 28},
-        {id: 4, name: 'Michael', age: 38},
-        {id: 5, name: 'William', age: 48},
-        {id: 6, name: 'David', age: 58},
-        {id: 7, name: 'Richard', age: 68},
-        {id: 8, name: 'Joseph', age: 78},
-        {id: 9, name: 'Thomas', age: 88},
-        {id: 10, name: 'Charles', age: 98}
-    ]
+    const Button1Foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
+    }
+    const Button2Foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
+    }
+    const Button3Foo = (text: string) => {
+        console.log(text)
+    }
+
     return (
         <div className="App">
-            <NewComponent students = {students} />
-            <TopCars topCars = {topCars} />
+            <Button name={'MyYouTubeChanel-1'}
+                    callback={() => Button1Foo('Vasya', 11)}/>
+            <Button name={'MyYouTubeChanel-2'}
+                    callback={() => Button2Foo('Ivan', 100)}/>
+            <Button name={'MyYouTubeChanel-3'}
+                    callback={() => Button3Foo('Im stupid button')}/>
         </div>
     )
 }
