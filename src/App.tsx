cdcb84a1eaea6ae2e189f91cd6bc098ed5d97729
Ruslan Rutkovsky <rus.rutkovsky@yandex.ru>
2023-12-20 22:34:48 +0300

    microtasks -map

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,17 @@
-import React, {MouseEvent, useState} from 'react';
+import React from 'react';
 import './App.css';
 import {Component} from './Components/Component';
 
-export type FilterType = 'All' | 'Dollars' | 'RUBLS';
-
 const App = () => {
-
-    const [money, setMoney] = useState([
-        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
-    ])
-
-    const [filter, setFilter] = useState<FilterType>('All')
-
-    let currentMoney = money;
-    if (filter === 'Dollars') {
-        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'Dollars')
-    }
-    if (filter === 'RUBLS') {
-        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS')
-    }
-
-    const onclickFilterHundler = (nameButton: FilterType) => {
-        setFilter(nameButton)
-    }
+    let data = [
+        {manufacturer:'BMW', model:'m5cs'},
+        {manufacturer:'Mercedes', model:'e63s'},
+        {manufacturer:'Audi', model:'rs6'}
+    ]
 
     return (
         <div className="App">
-            <Component currentMoney={currentMoney} onclickFilterHundler={onclickFilterHundler}/>
+            <Component data={data}/>
         </div>
     )
 }
--- src/Components/Component.tsx
+++ src/Components/Component.tsx
@@ -1,40 +1,20 @@
 import React from 'react';
-import {FilterType} from '../App';
 
-type MoneyProps = {
-    banknots: string
-    value: number
-    number: string
+type ComponentPropsType = {
+    data: DataPropsType[]
 }
 
-type PropsType = {
-    currentMoney: MoneyProps[]
-    onclickFilterHundler: (nameButton: FilterType) => void
+type DataPropsType = {
+    manufacturer: string
+    model: string
 }
 
-export const Component = (props:PropsType) => {
+export const Component = (props: ComponentPropsType) => {
     return (
         <div>
-            <ul>
-                {props.currentMoney.map((el, index) => {
-                        return (
-                            <li key={index}>
-                                <span>{el.banknots}</span>
-                                <span>{el.value}</span>
-                                <span>{el.number}</span>
-                            </li>
-                        )
-                    }
-                )
-                }
-            </ul>
-            <div style={{marginLeft: '35px'}}>
-                <button onClick={() => props.onclickFilterHundler('All')}>All</button>
-                <button onClick={() => props.onclickFilterHundler('RUBLS')}>RUBLS</button>
-                <button onClick={() => props.onclickFilterHundler('Dollars')}>Dollars</button>
-            </div>
+            {props.data.map((el, index) => <table><tr key={index + 1}>{el.manufacturer} {el.model}</tr></table>)}
         </div>
-     )
+    )
 }
 
 
