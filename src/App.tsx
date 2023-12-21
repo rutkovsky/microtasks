0ab714b38b3582926ffb40ec62e1aad2d2d32f42
Ruslan Rutkovsky <rus.rutkovsky@yandex.ru>
2023-12-21 22:32:47 +0300

    microtasks -filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Button} from './Components/Button';
+import {NewComponent} from './Components/NewComponent';
+
+type FilterType = 'all' | 'RUBLS' | 'Dollars'
 
 const App = () => {
-    let [A, setA] = useState(1)
-    const Foo1 = () => {
-        setA(A-1)
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+
+    let [filter, setFilter] = useState<FilterType>('all')
+
+    let currentMoney = money;
+    if (filter === 'RUBLS') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS')
     }
-    const Foo2 = () => {
-        setA(A+1)
+    if (filter === 'Dollars') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'Dollars')
     }
-    const Foo3 = () => {
-        setA(A=0)
+    let onClickButtonHandler = (nameButton: FilterType) => {
+        setFilter(nameButton)
     }
+
     return (
         <div className="App">
-            <div>{A}</div>
-            <Button name={'-'} onClickHandler={Foo1}/>
-            <Button name={'+'} onClickHandler={Foo2}/>
-            <Button name={'0'} onClickHandler={Foo3}/>
+            <NewComponent currentMoney={currentMoney} onClickButtonHandler={onClickButtonHandler}/>
         </div>
     )
 }
--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,12 +1,1 @@
 import React from 'react';
-
-type ButtonPropsType = {
-    name: string
-    onClickHandler: () => void
-}
-
-export const Button = (props: ButtonPropsType) => {
-    return (
-        <button onClick={props.onClickHandler}>{props.name}</button>
-    )
-}
